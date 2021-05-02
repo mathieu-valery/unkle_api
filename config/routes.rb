be5e204ae3d6755470fe4a54b3f5c602f885e7eb
Mathieu Valery <mathieuromainvalery@gmail.com>
2021-05-02 12:16:07 +0200

    sign in works

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
+      devise_scope :user do
+        post "sign_up", to: "registrations#create"
+        post "sign_in", to: "sessions#create"
+      end
       resources :users, only: [ :index ]
       resources :contracts, only: [ :index ]
     end
